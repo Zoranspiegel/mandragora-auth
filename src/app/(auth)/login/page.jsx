f6c994bb755c85ff3171daf16884909fa3d705d6
Juan José Rodriguez <zoranbow@gmail.com>
2025-06-03 16:15:39 -0500

    login & signup forms layout

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+import styles from "./LoginPage.module.css";
+import { FcGoogle } from "react-icons/fc";
+
 export default function LoginPage() {
   return (
-    <div>LOGIN</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="Email..." />
+        <div>
+          <input type="password" placeholder="Contraseña..." />
+          <span>
+            <Link href="/login">Olvidaste tu contraseña?</Link>
+          </span>
+        </div>
+        <div>
+          <button>Continuar</button>
+          <span>
+            No tienes una cuenta? <Link href="/signup">Registrarse</Link>
+          </span>
+        </div>
+      </form>
+      <div className={styles.o}>
+        <span>o</span>
+      </div>
+      <Link href="/home" className={styles.googleBtn}>
+        <FcGoogle fontSize="30px" />
+        Google
+      </Link>
+    </div>
   );
 }
